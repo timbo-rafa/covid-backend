bbb06cdfd904bbe04086e598e69583d4642b6443
Rafael Timbó <timbo.rafa@gmail.com>
2025-02-04 12:15:38 -0500

    adding id to column metadata

--- prisma/sql/getTableColumnNames.sql
+++ prisma/sql/getTableColumnNames.sql
@@ -1,9 +1,10 @@
 -- @param {String} $1:tableName the table name
 SELECT 
-    table_name AS "tableName",
-    column_name AS "columnName",
-    data_type AS "dataType"
-  FROM information_schema.columns
+  CONCAT(table_schema, '.', table_name, '.', column_name) AS "id",
+  table_name AS "tableName",
+  column_name AS "columnName",
+  data_type AS "dataType"
+FROM information_schema.columns
  WHERE table_schema NOT IN  ('pg_catalog', 'information_schema')
  AND table_name NOT IN ('_prisma_migrations')
  AND table_schema = 'public'
